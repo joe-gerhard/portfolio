8ddc7b41fcc82d7ccc67836d350fd9ce8a42bdba
Joe Gerhard <jlgerhard@gmail.com>
2023-12-18 19:01:03 -0600

    Change to IntersectionObserver in Project section

--- src/components/ProjectsSection/Project.jsx
+++ src/components/ProjectsSection/Project.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import {
     Description,
     Text,
@@ -10,35 +10,26 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
-import { useRef } from "react";
 
 const Project = ({ project, scrollY }) => {
-    const [hover, setHover] = useState(false);
-    const projectRef = useRef();
+    const [isIntersecting, setIsIntersecting] = useState(false);
+    const ref = useRef(null);
 
     useEffect(() => {
-        if (!projectRef.current) return;
-
-        const projectTop = projectRef.current.offsetTop;
-        const projectBottom = projectTop + projectRef.current.offsetHeight;
-
-        // if the scrollY position fallse within the project, set the hover condition to true
-        if (scrollY > projectTop - 400 && scrollY < projectBottom - 200) {
-            setHover(true);
-        } else {
-            setHover(false);
-        }
-    }, [scrollY]);
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setIsIntersecting(entry.isIntersecting);
+            },
+            { rootMargin: "-25%" },
+        );
+        observer.observe(ref.current);
+        return () => observer.disconnect();
+    }, []);
 
     return (
-        <StyledProject
-            ref={projectRef}
-            onMouseEnter={() => setHover(true)}
-            onMouseLeave={() => setHover(false)}
-        >
+        <StyledProject ref={ref}>
             <ImageContainer
-                hover={hover}
+                hover={isIntersecting}
                 onClick={() => window.open(project.link)}
             >
                 <img src={project.image} alt={project.name} />
@@ -55,7 +46,9 @@ const Project = ({ project, scrollY }) => {
                         {project.name}
                     </a>
                 </h2>
-                <Description hover={hover}>{project.description}</Description>
+                <Description hover={isIntersecting}>
+                    {project.description}
+                </Description>
                 <Technologies>
                     {project.technologies.map((tech) => (
                         <span key={tech}>{tech}</span>
